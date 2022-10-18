8432c91f2044f129e42c811c5bcf583b786c9aad
Maksymenko Maksym <maksymenko1992@ukr.net>
2022-10-18 16:03:55 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,8 @@ import styled from 'styled-components';
 
 const Div = styled.div`
   color: ${props => props.theme.colors.green};
+  font-family: ${props => props.theme.fonts.main};
+  font-size: ${props => props.theme.fontSizes.xl};
 `;
 
 export const App = () => {
