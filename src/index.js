c76b910c15ec5bcbbace5d1dd5f0be95db03fa4c
Maksymenko Maksym <maksymenko1992@ukr.net>
2022-10-18 15:47:39 +0300

    added libraries and theme

--- src/index.js
+++ src/index.js
@@ -2,9 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
+import './assets/fonts/fonts.css';
+import { ThemeProvider } from 'styled-components';
+import { theme } from 'utility/theme';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
