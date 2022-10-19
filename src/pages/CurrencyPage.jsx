3f36f36fdc0f9760f52e4d8c9392544d8ee445b6
Maksymenko Maksym <maksymenko1992@ukr.net>
2022-10-19 14:28:39 +0300

    added page for navigation& logo component

--- src/pages/CurrencyPage.jsx
+++ src/pages/CurrencyPage.jsx
@@ -1,9 +1,9 @@
 import Currency from 'components/Currency/Currency';
+import Navigation from 'components/Navigation/Navigation';
 // import Navigation from 'components/Navigation/Navigation';
 import React from 'react';
 // import MediaQuery from 'react-responsive';
 import { useMediaQuery } from 'react-responsive';
-// import { redirect } from 'react-router-dom';
 
 const CurrencyPage = () => {
   const isMobile = useMediaQuery({ query: '(max-width: 767px)' });
@@ -14,7 +14,7 @@ const CurrencyPage = () => {
       {/* <Navigation /> */}
       {isMobile && (
         <>
-          {/* <Navigation /> */}
+          <Navigation />
           <Currency />
         </>
       )}
