a209c9dd3f95e44ac378ad70ba14c61d320159cf
BogdanGlazkov <bglazkov@i.ua>
2022-10-19 13:13:12 +0300

    Update imports

--- src/components/TestCom/TestCom .jsx
+++ src/components/TestCom/TestCom .jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { RegistrationPage, LoginPage } from '../../pages';
+import TestModal from 'components/TestModal';
 const TestCom = () => {
-  return(
-  <>
-  <RegistrationPage/>
-  <LoginPage/>
-  </>
-)};
-
-
+  return (
+    <>
+      <RegistrationPage />
+      <LoginPage />
+      <TestModal />
+    </>
+  );
+};
 
 export default TestCom;
