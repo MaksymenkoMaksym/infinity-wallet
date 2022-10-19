2465b00a9a2ba7805b847a5fbd564ae02b6d8b98
Dmytro Stupak <stupakbf@gmail.com>
2022-10-19 13:42:41 +0300

    hell code

--- src/components/TestCom/TestCom .jsx
+++ src/components/TestCom/TestCom .jsx
@@ -1,7 +1,17 @@
+import Balance from 'components/Balance/Balance';
+import Currency from 'components/Currency/Currency';
+import Navigation from 'components/Navigation/Navigation';
+import MediaQuery from 'react-responsive';
 import React from 'react';
+import CurrencyPage from 'pages/CurrencyPage';
 
 const TestCom = () => {
-  return <div>TestCom </div>;
+  return (
+    <>
+      <Navigation />
+      <CurrencyPage />
+    </>
+  );
 };
 
 export default TestCom;
