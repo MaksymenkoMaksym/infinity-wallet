f652d39c3054cb78081874ae60862c6221a434b9
Maksymenko Maksym <maksymenko1992@ukr.net>
2022-10-19 16:00:37 +0300

    Update HomePage.jsx

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,8 +1,9 @@
 import Balance from 'components/Balance/Balance';
 import Chart from 'components/Chart';
 import Currency from 'components/Currency/Currency';
-import { Table } from 'components/Currency/Currency.styled';
+
 import Navigation from 'components/Navigation/Navigation';
+import Table from 'components/Table';
 
 // import MediaQuery from 'react-responsive';
 // import { useMediaQuery } from 'react-responsive';
