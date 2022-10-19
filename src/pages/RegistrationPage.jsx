0cdbd1510ac13bf5c5d9ad3d4675b0cb515422de
seacat271 <seacat271083@gmail.com>
2022-10-19 15:35:07 +0300

    add style

--- src/pages/RegistrationPage.jsx
+++ src/pages/RegistrationPage.jsx
@@ -1,12 +1,20 @@
 import {RegistrationForm} from '../components/Forms';
 import { Section } from './common.styled';
 import Logo from 'components/Logo/Logo';
+import { useMediaQuery } from 'react-responsive'
 
 
 export const RegistrationPage = () => {
+const isBigScreen = useMediaQuery({ minWidth: 1200 })
+const isTabletOrMobile = useMediaQuery({ maxWidth: 768 })
+const isMobile = useMediaQuery({maxWidth: 480})
+const isRetina = useMediaQuery({ minResolution: 2})
+
     return (
-    <Section>
-        <Logo/>
-        <RegistrationForm/>
-    </Section>
+    <div>
+        <Section>
+            <Logo/>
+            <RegistrationForm/>
+        </Section>
+    </div>
 )}
--- src/pages/common.styled.jsx
+++ src/pages/common.styled.jsx
@@ -1,9 +1,21 @@
 import styled from "styled-components";
 
+
+
 export const Section = styled.section`
 padding: 20px;
 height: 100vh;
 width: 280px;
 margin-right: auto; 
 margin-left: auto;
+    @media (min-width: 768px) {
+    width: 533px;
+    height: 468px;
+    border-radius: 20px;
+    min-width: auto;
+    min-height: auto;
+    overflow: hidden;
+  }
 `
+
+
