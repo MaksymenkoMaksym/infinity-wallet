fa59a92390bcdab0ac480bfdfabfae1357a0837b
seacat271 <seacat271083@gmail.com>
2022-10-19 13:32:14 +0300

    fix selector

--- src/components/Forms/RegistrationForm.styled.jsx
+++ src/components/Forms/RegistrationForm.styled.jsx
@@ -17,7 +17,15 @@ left: 50px;
 font-size: 18px;
 transition: color 250ms linear, transform 250ms linear, font-size 250ms linear;
 `
-
+export const IconSvg = styled.svg`
+width: 24px;
+height: 24px;
+fill: currentColor;
+position: absolute;
+top: -7px;
+left: 10px;
+transition:  color 250ms linear;
+`
 
 export const Input = styled(Field)`
 /* width: 100%; */
@@ -32,28 +40,22 @@ transition: border-color 250ms linear;
 &:focus, &:not(:placeholder-shown){
     border-bottom: 1px solid #000000;  
 }
-&:focus+.svg,
-&:not(:placeholder-shown)+.svg {
+&:focus + ${IconSvg},
+&:not(:placeholder-shown) + ${IconSvg} {
     color: #000000;
 }
 
-&:focus~.placeholder,
-&:not(:placeholder-shown)~.placeholder {
+&:focus~ ${Placeholder},
+&:not(:placeholder-shown)~ ${Placeholder} {
     color: #000000;
     transform: translateY(-20px);
     font-size: 12px;
 }
 `
 
-export const IconSvg = styled.svg`
-width: 24px;
-height: 24px;
-fill: currentColor;
-position: absolute;
-top: -7px;
-left: 10px;
-transition:  color 250ms linear;
-`
+
 
 export const Button = styled.button`
+
+
 `
