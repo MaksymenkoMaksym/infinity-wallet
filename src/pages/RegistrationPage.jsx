d4d481d09784b5fa6416a8a00094fe9f07876b5e
seacat271 <seacat271083@gmail.com>
2022-10-21 01:34:04 +0300

    Update RegistrationPage.jsx

--- src/pages/RegistrationPage.jsx
+++ src/pages/RegistrationPage.jsx
@@ -2,12 +2,8 @@ import { Section, Backdrop} from '../pages/common.styled';
 import Logo from 'components/Logo/Logo';
 import {RegistrationForm} from '../components/Forms';
 import { Body} from './common.styled';
-
 import { TitleThumb } from 'components/TitleThumb/TitleThumb';
 import { useMediaQuery } from 'react-responsive';
-
-
-
 
 export const RegistrationPage = () => {
 const isTabletOrBigScreen = useMediaQuery({minWidth: 768})
