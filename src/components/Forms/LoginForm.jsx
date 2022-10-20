cc9f905d3cd3ec41abd4648ff27d0ff7c00df91e
Maksymenko Maksym <maksymenko1992@ukr.net>
2022-10-20 13:04:26 +0300

    add registration logic

--- src/components/Forms/LoginForm.jsx
+++ src/components/Forms/LoginForm.jsx
@@ -1,38 +1,49 @@
-import { Formik} from "formik";
-import * as yup from 'yup';
-import svgIcon from "../../assets/images/icons.svg"
-import {
-    Input,
-    Label,
-    Placeholder,
-    IconSvg, Button, ActiveButton, StyledForm
-  } from './RegistrationForm.styled';
+import { useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { Formik } from 'formik';
 
+import * as yup from 'yup';
+import svgIcon from '../../assets/images/icons.svg';
+import {
+  Input,
+  Label,
+  Placeholder,
+  IconSvg,
+  Button,
+  ActiveButton,
+  StyledForm,
+} from './RegistrationForm.styled';
+import { loginUser } from 'redux/auth/authOperation';
 
 export const LoginForm = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
-    const initialValues = {
-        email: "",
-        password: "",
-    };
+  const initialValues = {
+    email: '',
+    password: '',
+  };
 
-    let schema = yup.object().shape({
-        email: yup.string().email().required(),
-        password: yup.number().required().positive().integer(),
-    });
+  let schema = yup.object().shape({
+    email: yup.string().email().required(),
+    password: yup.number().required().positive().integer(),
+  });
 
-    const handleSubmit = (values, {resetForm}) => {
-        console.log(values)
-        resetForm()
-        }
+  const handleSubmit = (values, { resetForm }) => {
+    if (!values.password && values.email) {
+      return alert('password || email are empty');
+    }
+    dispatch(loginUser(values));
+    resetForm();
+  };
 
-    return (
+  return (
     <Formik
-        initialValues={initialValues}
-        onSubmit={handleSubmit}
-        validate={schema}
+      initialValues={initialValues}
+      onSubmit={handleSubmit}
+      validate={schema}
     >
-        <StyledForm>
+      <StyledForm>
         <Label name="email">
           <Input type="email" name="email" placeholder=" " />
           <IconSvg className="svg">
@@ -47,11 +58,11 @@ export const LoginForm = () => {
           </IconSvg>
           <Placeholder className="placeholder">Password</Placeholder>
         </Label>
-            <ActiveButton type="submit">LOG IN</ActiveButton>
-            <Button type="submit">REGISTER</Button>
-        </StyledForm>
+        <ActiveButton type="submit">LOG IN</ActiveButton>
+        <Button type="button" onClick={navigate('/registration')}>
+          REGISTER
+        </Button>
+      </StyledForm>
     </Formik>
-)} 
-
-
-
+  );
+};
