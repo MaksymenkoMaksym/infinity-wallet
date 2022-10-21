afb02cd430a3ccfb67155a7049b788755a5198a7
BogdanGlazkov <bglazkov@i.ua>
2022-10-21 17:55:19 +0300

    Fix bg image

--- src/components/Container/Container.styled.js
+++ src/components/Container/Container.styled.js
@@ -1,8 +1,5 @@
-// import VioletSpotTab from '../../assets/images/bg-bottom-md.png';
-import VioletSpotDesc from '../../assets/images/bg-bottom-lg.png';
-// import PinkSpotTab from '../../assets/images/bg-top-md.png';
-import PinkSpotDesc from '../../assets/images/bg-top-lg.png';
-const { default: styled } = require('styled-components');
+import BlurImg from '../../assets/images/rectangle.png';
+import styled from 'styled-components';
 
 const Container = styled.div`
   outline: 1px solid red;
@@ -18,15 +15,9 @@ const Container = styled.div`
   ${props => props.theme.breakpoints.desc} {
     padding: 0 18px;
     max-width: 1280px;
-    /*background-image: linear-gradient(
-        to right,
-        rgba(255, 255, 255, 0.4),
-        rgba(255, 255, 255, 0.1)
-      ),
-      url(${PinkSpotDesc}), url(${VioletSpotDesc});
-    background-repeat: no-repeat, no-repeat;
-    background-position: right top, right top, left bottom;
-    backdrop-filter: blur(25px);*/
+    background-image: url(${BlurImg});
+    background-repeat: no-repeat;
+    background-size: cover;
   }
 `;
 
