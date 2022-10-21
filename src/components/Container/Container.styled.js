62810cd8304438e9aec789cfe4e7a4c3c19cb116
BogdanGlazkov <bglazkov@i.ua>
2022-10-21 19:51:36 +0300

    Add tablet blur background

--- src/components/Container/Container.styled.js
+++ src/components/Container/Container.styled.js
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
 import BlurImg from '../../assets/images/rectangle.png';
+import BlurImgMd from '../../assets/images/rectangle_md.png';
 
 const Container = styled.div`
   padding: 0 ${props => props.theme.space[2]}px;
@@ -10,13 +11,14 @@ const Container = styled.div`
   ${props => props.theme.breakpoints.tab} {
     padding: 0 ${props => props.theme.space[4]}px;
     max-width: 768px;
+    background-image: url(${BlurImgMd});
+    background-repeat: no-repeat;
+    background-size: cover;
   }
   ${props => props.theme.breakpoints.desc} {
     padding: 0 18px;
     max-width: 1280px;
     background-image: url(${BlurImg});
-    background-repeat: no-repeat;
-    background-size: cover;
   }
 `;
 
