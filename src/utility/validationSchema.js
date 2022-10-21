d128b4a69af155525766fe3a48ef893f1fbba8f2
seacat271 <seacat271083@gmail.com>
2022-10-21 14:12:25 +0300

    capitalize first char

--- src/utility/validationSchema.js
+++ src/utility/validationSchema.js
@@ -1,22 +1,22 @@
 import * as Yup from 'yup';
 
 export const validationSchemaLogin = Yup.object().shape({
-    email: Yup.string().email('incorrect email').required('missing email'),
+    email: Yup.string().email('Incorrect email').required('Missing email'),
     password: Yup.string()
-      .min(6, 'password should be 8 chars minimum')
-      .max(12, 'password should be 12 chars maximum')
-      .required('missing password'),
+      .min(6, 'Password should be 8 chars minimum')
+      .max(12, 'Password should be 12 chars maximum')
+      .required('Missing password'),
   });
 
 export const validationSchemaRegister = Yup.object()
     .shape({
       confirmPassword: Yup.string()
-        .min(6, 'password should be 8 chars minimum')
-        .max(12, 'password should be 12 chars maximum')
-        .required('missing confirm password'),
+        .min(6, 'Password should be 8 chars minimum')
+        .max(12, 'Password should be 12 chars maximum')
+        .required('Missing confirm password'),
       firstName: Yup.string()
         .min(1)
-        .max(12, 'first name should be 12 chars maximum.')
-        .required('missing first name'),
+        .max(12, 'First name should be 12 chars maximum.')
+        .required('Missing first name'),
     })
     .concat(validationSchemaLogin);
