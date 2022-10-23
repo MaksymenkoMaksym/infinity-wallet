8e70821c669f4dbcd890b4f8ace6bcbf61e9177a
Anastasiia <106449970+Anastasiia1002@users.noreply.github.com>
2022-10-23 23:24:00 +0300

    add useMediaQuery

--- src/components/DashboardPage/DashboardPage.js
+++ src/components/DashboardPage/DashboardPage.js
@@ -1,25 +1,25 @@
 import { useSelector } from 'react-redux';
-import { Mob, Tab } from 'components/MediaWraper/MediaWraper';
+// import { Mob, Tab } from 'components/MediaWraper/MediaWraper';
 import MobileDashboard from './MobileDashboard';
 import LaptopDashboard from './LaptopDashboard';
 import Loader from 'components/Loader';
-
+import { useMediaQuery } from 'react-responsive';
 import { UlBoard } from './DashboardPage.styled';
 import { selectIsLoadingTransaction } from 'redux/transactions/transactionsSelectors';
+
 const DashboardPage = () => {
   const isLoadingTransaction = useSelector(selectIsLoadingTransaction);
+  const isTabletOrBigScreen = useMediaQuery({ minWidth: 768 });
+  const isMobileScreen = useMediaQuery({ maxWidth: 767 });
+
   return (
     <div key={Math.random() * 100}>
       {isLoadingTransaction ? (
         <Loader />
       ) : (
         <UlBoard>
-          <Mob>
-            <MobileDashboard />
-          </Mob>
-          <Tab>
-            <LaptopDashboard />
-          </Tab>
+          {isMobileScreen && <MobileDashboard />}
+          {isTabletOrBigScreen && <LaptopDashboard />}
         </UlBoard>
       )}
     </div>
