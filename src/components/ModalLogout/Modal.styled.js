b48233b9506bce7b1609e08b258ea41a456724fd
seacat271 <seacat271083@gmail.com>
2022-10-23 21:09:53 +0300

    fix modal

--- src/components/ModalLogout/Modal.styled.js
+++ src/components/ModalLogout/Modal.styled.js
@@ -28,6 +28,7 @@ export const ModalStyled = styled.div`
 export const ModalContent = styled.div`
   width: 500px;
   padding: 20px;
+  border-radius: ${props => props.theme.radii.md};
   background-color: ${props => props.theme.colors.white};
   transition: all 0.3s ease-in-out;
   transform: translateY(-200px);
@@ -43,6 +44,7 @@ export const ModalContent = styled.div`
 
 export const ModalTitle = styled.h3`
   text-align: center;
+  font-size: ${props => props.theme.fontSizes.xl};
 `;
 
 export const ModalFooter = styled.div`
@@ -53,21 +55,21 @@ export const ModalFooter = styled.div`
 `;
 
 export const ModalButton = styled.button`
-  width: 80px;
-  height: 30px;
-
-  border: 1px solid ${props => props.theme.colors.black};
-  border-radius: ${props => props.theme.space[1]}px;
-
-  color: ${props => props.theme.colors.black};
-  background-color: ${props => props.theme.colors.grayIcon};
-
+  width: 140px;
+  height: 50px;
+  font-size: ${props => props.theme.fontSizes.xl};
+  color: ${props => props.theme.colors.white};
+  background-color: ${props => props.theme.colors.green};
+  border: ${props => props.theme.borders.green};
+  border-radius: ${props => props.theme.radii.md};
+ 
   cursor: pointer;
   transition: all 0.4s ease-in-out;
 
   &:hover {
-    box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
-    color: ${props => props.theme.colors.white};
-    background-color: ${props => props.theme.colors.blue};
+        color: ${props => props.theme.colors.white};
+        box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
+        background-color: ${props => props.theme.colors.blue};
+        border-color: ${props => props.theme.colors.blue};
   }
 `;
